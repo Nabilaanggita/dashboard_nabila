75a952af4b9818bb354cb654596eda74f4fdd54b
Nabilaanggita <nabilaanggita2281@gmail.com>
2024-10-04 15:28:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,104 +2,127 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from streamlit_option_menu import option_menu
-with st.sidebar:
-    selected = option_menu(
-        menu_title="Main Menu",  # Required title for the menu
-        options=["Home", "Projects", "Contact"],  # Options to display in the menu
-    )
-    if selected == "Home":
-    st.title(f"You have selected {selected}")
+import seaborn as sns
+import datetime as dt
+import calendar
 
-    if selected == "Projects":
-        st.title(f"You have selected {selected}")
 
-    if selected == "Contact":
-        st.title(f"You have selected {selected}")
-
-#Gathering 2 data day and hour
+#Gathering 2 data day and hour dashboard/data_siap.csvdashboard/data_siap.csv
+df = pd.read_csv("data/data/data_siap.csv")
 day_df = pd.read_csv("data/data/day.csv")
 hour_df = pd.read_csv("data/data/hour.csv")
 
-new_order_df = pd.merge(
-    left=day_df,
-    right=hour_df,
-    how="inner",
-    left_on="instant",
-    right_on="instant"
-)
-# Judul dashboard
-st.title("Dashboard New Order")
-st.write("Data New Order adalah data hour digabungkan dengan data day")
-# Menampilkan DataFrame
-st.write("## Data New Order")
-st.dataframe(new_order_df)
+# Navigation bar with "Beranda" (Home) and "Filter" options
+selected_page = st.sidebar.selectbox(
+    "Pilih Halaman (Select Page)",
+    options=["Beranda (Home)", "Filter (Filter)"])
 
-# Statistik deskriptif
-st.write("## Statistik Deskriptif")
-st.write(new_order_df.describe())
+if selected_page == "Beranda (Home)":  
+    # Judul dashboard
+    st.title("Dashboard ")
+    st.write("Data Penyewa sepeda")
+    # Menampilkan DataFrame
+    st.write("## Data hasil ")
+    st.dataframe(df)
 
-#Buat filter memunculkan fitur yang diinginkan
+    # Statistik deskriptif
+    st.write("## Statistik Deskriptif")
+    st.write(df.describe())
+
+
+#filter memunculkan fitur yang diinginkan
 # Filter items dengan selectbox
-selected_filters = st.multiselect(
-    "Pilih Filter",
-    options=["temp", "atemp", "weathersit", "year"],
+elif selected_page == "Filter (Filter)":
+    selected_filters = st.multiselect(
+        "Pilih Filter",
+        options=["temp", "atemp", "weathersit", "year"],
+    )
+
+    if "temp" in selected_filters:
+        st.write("## Melihat Data Berdasarkan `temp`")
+        st.write(df.groupby(by="temp").agg({
+            "instant": "nunique",
+            "cnt": ["max", "min", "mean", "std"]}))
+        st.write("# Pengaruh Temp terhadap cnt (jumlah penyewa)")
+        fig, ax = plt.subplots()
+        temp_means = df.groupby('temp')['cnt'].mean()
+        ax.bar(temp_means.index, temp_means.values)
+        ax.set_xlabel("Temp")
+        ax.set_ylabel("Rata-rata cnt")
+        st.pyplot(fig)
+        st.write("pada temperatur tinggi terdapat banyak penyewa, sedangka apabila temperatur rendah sedikit")
+        # tampilkan histogram tiap bulannya
+        plt.figure(figsize=(9,6))
+        sns.boxplot(
+            x="mnth",
+            y="cnt",
+            data=df,
+            palette=["blue", "yellow", "purple", "pink", "red"]
+        )
+
+        plt.xlabel("Month")
+        plt.ylabel("Total Rides")
+        plt.title("Count by Year")
+        plt.show()
+
+
+    if "atemp" in selected_filters:
+        st.write("## Melihat Data Berdasarkan `atemp`")
+        st.write(df.groupby(by="atemp").agg({
+            "instant": "nunique",
+            "cnt": ["max", "min", "mean", "std"]}))
+        st.write("# Pengaruh Atemp terhadap cnt (jumlah penyewa)")
+        fig, ax = plt.subplots()
+        atemp_means = df.groupby('atemp')['cnt'].mean()
+        ax.bar(atemp_means.index, atemp_means.values)
+        ax.set_xlabel("atemp")
+        ax.set_ylabel("Rata-rata cnt")
+        st.pyplot(fig)
+
+    if "weathersit" in selected_filters:
+        st.write("## Melihat Data Berdasarkan `weathersit`")
+        st.write("1. Clear, Few clouds, Partly cloudy")
+        st.write("2. Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist")
+        st.write("3. Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds")
+        st.write("4. Heavy Rain + Ice Pellets + Thunderstorm + Mist, Snow + Fog")
+        st.write(df.groupby(by="weathersit").agg({
+            "instant": "nunique",
+            "cnt": ["max", "min", "mean", "std"]}))
+        st.write("# Pengaruh Weathersit terhadap cnt(jumlah penyewa)")
+        fig, ax = plt.subplots()
+        weathersit_means = df.groupby('weathersit')['cnt'].mean()
+        ax.bar(weathersit_means.index, weathersit_means.values)
+        ax.set_xlabel("Weathersit")
+        ax.set_ylabel("Rata-rata cnt")
+        st.pyplot(fig)
+
+    if "year" in selected_filters:
+        st.write("## Melihat Data Berdasarkan `yr`")
+        st.write(df.groupby(by="yr").agg({
+            "instant": "nunique",
+            "cnt": ["max", "min", "mean", "std"]}))
+        st.write("# Pengaruh Tahun terhadap cnt (jumlah penyewa)")
+        fig, ax = plt.subplots()
+        year_means = df.groupby('yr')['cnt'].mean()
+        ax.bar(year_means.index, year_means.values)
+        ax.set_xlabel("Tahun")
+        ax.set_ylabel("Rata-rata cnt")
+        st.pyplot(fig)
+
+#menampilkan data
+df.describe()
+df.head()
+
+# tampilkan histogram tiap minggunya
+plt.figure(figsize=(9,6))
+sns.lineplot(
+    x="weekday",
+    y="cnt",
+    data=df,
+    palette=["navy","aqua", "yellow", "orange", "pink","magenta"]
 )
 
-if "temp" in selected_filters:
-    st.write("## Melihat Data Berdasarkan `temp_y`")
-    st.write(new_order_df.groupby(by="temp_y").agg({
-        "instant": "nunique",
-        "cnt_y": ["max", "min", "mean", "std"]}))
-    st.write("# Pengaruh Temp terhadap cnt (jumlah penyewa)")
-    fig, ax = plt.subplots()
-    temp_means = new_order_df.groupby('temp_y')['cnt_y'].mean()
-    ax.bar(temp_means.index, temp_means.values)
-    ax.set_xlabel("Temp")
-    ax.set_ylabel("Rata-rata cnt")
-    st.pyplot(fig)
-
-if "atemp" in selected_filters:
-    st.write("## Melihat Data Berdasarkan `atemp_y`")
-    st.write(new_order_df.groupby(by="atemp_y").agg({
-        "instant": "nunique",
-        "cnt_y": ["max", "min", "mean", "std"]}))
-    st.write("# Pengaruh Atemp terhadap cnt (jumlah penyewa)")
-    fig, ax = plt.subplots()
-    atemp_means = new_order_df.groupby('atemp_y')['cnt_y'].mean()
-    ax.bar(atemp_means.index, atemp_means.values)
-    ax.set_xlabel("atemp")
-    ax.set_ylabel("Rata-rata cnt")
-    st.pyplot(fig)
-
-if "weathersit" in selected_filters:
-    st.write("## Melihat Data Berdasarkan `weathersit_y`")
-    st.write("1. Clear, Few clouds, Partly cloudy")
-    st.write("2. Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist")
-    st.write("3. Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds")
-    st.write("4. Heavy Rain + Ice Pellets + Thunderstorm + Mist, Snow + Fog")
-    st.write(new_order_df.groupby(by="weathersit_y").agg({
-        "instant": "nunique",
-        "cnt_y": ["max", "min", "mean", "std"]}))
-    st.write("# Pengaruh Weathersit terhadap cnt(jumlah penyewa)")
-    fig, ax = plt.subplots()
-    weathersit_means = new_order_df.groupby('weathersit_y')['cnt_y'].mean()
-    ax.bar(weathersit_means.index, weathersit_means.values)
-    ax.set_xlabel("Weathersit")
-    ax.set_ylabel("Rata-rata cnt")
-    st.pyplot(fig)
-
-if "year" in selected_filters:
-    st.write("## Melihat Data Berdasarkan `yr_y`")
-    st.write(new_order_df.groupby(by="yr_y").agg({
-        "instant": "nunique",
-        "cnt_y": ["max", "min", "mean", "std"]}))
-    st.write("# Pengaruh Tahun terhadap cnt (jumlah penyewa)")
-    fig, ax = plt.subplots()
-    year_means = new_order_df.groupby('yr_y')['cnt_y'].mean()
-    ax.bar(year_means.index, year_means.values)
-    ax.set_xlabel("Tahun")
-    ax.set_ylabel("Rata-rata cnt")
-    st.pyplot(fig)
-
-
+plt.xlabel("weekday")
+plt.ylabel("Total Rides")
+plt.title("Count by weekday")
+plt.show()
